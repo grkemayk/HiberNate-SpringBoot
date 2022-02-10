a78a64bfada892a64dc41d43da20d81cc2b5f78a
WISERTECH\gorkem.aykac <grkemaykac1997@hotmail.com>
2022-02-10 21:54:24 +0300

    Commit

--- lambda-java-example/src/main/java/main.java
+++ lambda-java-example/src/main/java/main.java
@@ -3,28 +3,27 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.print("BASLADI\n");
-		 Configuration config = new Configuration();
-		 config.configure();
-		 SessionFactory sessionFactory = config.buildSessionFactory();
-		 Session session = sessionFactory.getCurrentSession();
-			try {
-				Student student = new Student("Grkem12","Aykac12", "mail@mail.com12");
-				session.beginTransaction();
-				session.save(student);
-				System.out.print("asdasdasd");
-				//session.delete(student);
-				//session.update(student);
-				//Student xStudent = (Student)session.get(Student.class, 163840);
-				
-				session.getTransaction().commit();
-				
-				
-			} finally {
-				sessionFactory.close();
-			}	
+		Configuration config = new Configuration();
+		config.configure();//Config dosyası okunur
+		SessionFactory sessionFactory = config.buildSessionFactory();
+		Session session = sessionFactory.getCurrentSession();//DB ve tablolar olusturulur
+		try 
+		{
+			session.beginTransaction();
 			
-				}
+			Student student = new Student("Grkem12","Aykac12", "mail@mail.com12");
+			session.save(student);
+		
+			
+			session.getTransaction().commit();//Queryleri gönderir
+		} 
+		finally 
+		{
+			sessionFactory.close();
+		}							
+	}
 
 }
